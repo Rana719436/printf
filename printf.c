ae09894b2a43638974ab226f994199604f49b81f
Eman-salem123 <emanzatonaa@gmail.com>
2023-09-16 14:28:12 -0700

    lol

---
+++ printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int _printf(const char *format)
+{
+        int i;
+
+        for(i =0; format[i] != '\0';i++)
+        {
+                putchar(format[i]);
+        }
+        return (0);
+}
