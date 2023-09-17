bc27154c172f19a52b76e1dd8c12c79fa887e18d
rana reda roshdy <ranaredaroshdy@gmail.com>
2023-09-16 17:13:37 -0700

    =====l

--- printf.c
+++ printf.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include "main.h"
 
 int _printf(const char *format, ...)
 {
+	int i;
 	va_list args;
 
 	va_start(args, format);
-	int i;
-
 	for (i = 0; format[i] != '\0'; i++)
-	{ 
+	{
 		_putchar(format[i]);
 	}
+	va_end(args);
 	return (i);
 }
