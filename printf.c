0540da85464084060fc4c73bc2b31e72041ef2b2
rana reda roshdy <ranaredaroshdy@gmail.com>
2023-09-17 16:16:22 -0700

    =====l

--- printf.c
+++ printf.c
@@ -26,16 +26,12 @@ int _printf(const char *format, ...)
 			    case 's':
 				    printf("%s", va_arg(args, char*));
 				    break;
+			    case '%':
+				    printf("%%");
+				    break;
 			    default:
 				    printf("Invalid format specifier");
 				    break;
-		    }
-		    i++;
-                    switch (format[i])
-		    {
-			    case '%':
-                                    printf("%%");
-                                    break;
 		    }
 	    }
 	    else
