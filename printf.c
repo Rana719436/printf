984e19dbe7e320e7b029009f35b8ad7fbddc0284
Eman-salem123 <emanzatonaa@gmail.com>
2023-09-16 17:38:15 -0700

    lol

--- printf.c
+++ printf.c
@@ -10,7 +10,25 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		_putchar(format[i]);
+		if (*format == '%)
+		{
+			format++
+			switch (*format)
+			{
+				case 'c':
+					printf("%c", va_arg(args, int));
+					break;
+				case 's':
+					printf("%s", va_arg(args, *char));
+				default:
+				       printf("you are stupid")
+				       break;	       
+			}
+		else
+		{
+			_putchar(format[i]);
+		}
+		}
 	}
 	va_end(args);
 	return (i);
