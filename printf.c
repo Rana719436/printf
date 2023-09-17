da7709cb070306ae44e6f9e119fbab57a8127021
Eman-salem123 <emanzatonaa@gmail.com>
2023-09-16 17:54:09 -0700

    lol

--- printf.c
+++ printf.c
@@ -4,32 +4,33 @@
 
 int _printf(const char *format, ...)
 {
-	int i;
-	va_list args;
+    int i;
+    va_list args;
 
-	va_start(args, format);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (*format == '%)
-		{
-			format++
-			switch (*format)
-			{
-				case 'c':
-					printf("%c", va_arg(args, int));
-					break;
-				case 's':
-					printf("%s", va_arg(args, *char));
-				default:
-				       printf("you are stupid")
-				       break;	       
-			}
-		else
-		{
-			_putchar(format[i]);
-		}
-		}
-	}
-	va_end(args);
-	return (i);
+    va_start(args, format);
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            switch (format[i])
+            {
+                case 'c':
+                    printf("%c", va_arg(args, int));
+                    break;
+                case 's':
+                    printf("%s", va_arg(args, char*));
+                    break;
+                default:
+                    printf("Invalid format specifier");
+                    break;
+            }
+        }
+        else
+        {
+            putchar(format[i]);
+        }
+    }
+    va_end(args);
+    return (i);
 }
